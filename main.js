eca70dbe4ec7246c197a06fa840196669753b06c
rofwell <elliot@peers.net.au>
2017-06-08 21:41:57 +1000

    Added whole project
    Whoops

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
+const ipc = electron.ipcMain;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
+const dialog = electron.dialog;
 
 const path = require('path')
 const url = require('url')
@@ -11,9 +14,76 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+const template = [
+  {
+    label:'Title',
+    submenu: [
+      {
+        label:'Quit',
+        role:'quit'
+      }
+    ]
+  },
+  {
+    label: 'Settings',
+    submenu: [
+      {
+        label:'Change Image',
+        click: function() {
+          dialog.showOpenDialog({ filters: [
+            { name: 'image', extensions: ['png','jpg','jpeg'] }
+            ]},
+            function(fileNames) {
+              if (fileNames === undefined) return;
+
+              var filename = fileNames[0];
+
+              mainWindow.webContents.send('new-image',url.format({
+                pathname: filename,
+                protocol: 'file:',
+                slashes: true
+              }));
+          });
+        }
+      },
+      {type:'separator'},
+      {
+        label: 'Less Often',
+        accelerator: 'CmdOrCtrl+Plus',
+        click: function() {
+          mainWindow.webContents.send('plus-cooldown');
+        }
+      },
+      {
+        label: 'More Often',
+        accelerator: 'CmdOrCtrl+-',
+        click: function() {
+          mainWindow.webContents.send('minus-cooldown');
+        }
+      },
+      {type:'separator'},
+      {
+        label: 'Longer Visible',
+        accelerator: 'CmdOrCtrl+Option+Plus',
+        click: function() {
+          mainWindow.webContents.send('plus-time');
+        }
+      },
+      {
+        label: 'Shorter Visible',
+        accelerator: 'CmdOrCtrl+Option+-',
+        click: function() {
+          mainWindow.webContents.send('minus-time');
+        }
+      }
+    ]
+  }
+]
+
 function createWindow () {
+
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 0, height: 0, frame: false, transparent: true, resizable: false, alwaysOnTop: true})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -25,6 +95,7 @@ function createWindow () {
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
+  mainWindow.setPosition(0,0)
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
@@ -32,6 +103,9 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+  
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
 }
 
 // This method will be called when Electron has finished
@@ -58,3 +132,6 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+ipc.on('window-move',(event,arg) => {
+  mainWindow.setPosition(arg[0],arg[1])
+});
--- renderer.js
+++ renderer.js
@@ -1,3 +1,59 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const ipc = require('electron').ipcRenderer;
+const escr = require('electron').screen;
+const remote = require('electron').remote;
+
+var imgElement = document.getElementsByTagName('img')[0];
+imgElement.onload = function() {
+    //alert('new-image called, img width/height=' + [imgElement.naturalWidth,imgElement.naturalHeight].toString());
+    remote.getCurrentWindow().setSize(imgElement.width,imgElement.height);
+}
+
+var settings = {
+    opacityOut: 300,
+    opacityIn: 100,
+    cooldown: 2000
+};
+
+ipc.on('new-image',function(event,arg) {
+    imgElement.src = arg;
+});
+
+ipc.on('plus-cooldown',function(event,arg) {
+    settings["cooldown"] += 200;
+});
+
+ipc.on('minus-cooldown',function(event,arg) {
+    if(settings["cooldown"] > 0) {
+        settings["cooldown"] -= 200;
+    }
+});
+
+ipc.on('plus-time',function(event,arg) {
+    settings["opacityOut"] += 100;
+});
+
+ipc.on('minus-time',function(event,arg) {
+    if(settings["opacityOut"] > 0) {
+        settings["opacityOut"] -= 100;
+    }
+});
+
+function moveToRandomPosition () {
+    var size = escr.getPrimaryDisplay().size
+
+    document.getElementsByTagName('img')[0].style.opacity = 0;
+    setTimeout(function(){
+        ipc.send('window-move',[Math.round(Math.random() * (size.width - imgElement.width)),Math.round(Math.random() * (size.height - imgElement.height))])
+        document.getElementsByTagName('img')[0].style.opacity = 1;
+        setTimeout(function(){
+            document.getElementsByTagName('img')[0].style.opacity = 0;
+        },settings["opacityOut"]);
+    },settings["opacityIn"])
+
+    setTimeout(moveToRandomPosition,(Math.random() * settings["cooldown"]) + (settings["opacityOut"] + settings["opacityIn"]));
+}
+
+setTimeout(moveToRandomPosition, 1000);
